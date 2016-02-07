7996f0da93ce6f6dcadcc157eace91b9d4e95c91
westerhack <westerhack@gmail.com>
2016-02-06 17:48:34 -0800

    matr is working, ish. i screwed something up somewhere, and its not keeping track of the filename...

--- matr.py
+++ matr.py
@@ -7,36 +7,37 @@ except ImportError:
 from numpy import matrix as npmatr
 class matr(npmatr):
 
-    def __new__(self, data = []):
-        return super().__new__(self,data)
+    defaultskipchar = '#'
 
-    def _frominp(inp):
-        data = []
-        if __debug__:
-            assert hasattr(inp, '__iter__') or hasattr(inp, '__next__')
-        if hasattr(inp, '__iter__'):
-            for line in inp:
-                data.append(line)
-        elif hasattr(inp, '__next__'):
-            while inp.hasnext():
-                data.append(next(inp))
-        return matr(data)
+    def __new__(self, file = None, data = [], skipchar = None):
+        if file:
+            ret = matr().__rrshift__(file)
+            ret.file = file
+            print('ret:',ret)
+            return ret
+        return super().__new__(self, data)
 
+    def __init__(self, data = None, file = None):
+        self.file = file
+
+    def __str__(self):
+        return 'file \'{}\':\n{}'.format(self.file, super().__str__())
     def __lshift__(self, inp):
         return self.__rrshift__(inp)
 
     def __rlshift__(self, outp):
         return self.__rshift__(outp)
 
-    def __rrshift__(self, inp):
-        """ file >> self :: Sets self.data to the data read from inp."""
-        if isinstance(inp, str):
-            return csv.reader(open(inp)) >> self
-        elif not hasattr(inp, '__iter__') and not hasattr(inp, '__next__'):
+    def __rrshift__(self, iterable, skipchar = None):
+        """ inputfile >> self :: Sets self to the matrix read from the input file"""
+        if isinstance(iterable, str):
+            return self.__rrshift__(csv.reader(open(iterable)))
+        elif not hasattr(iterable, '__iter__') and not hasattr(iterable, '__next__'):
             return NotImplemented
-        return matr._frominp(inp)
+        return matr(data = matr._npmatrfromfile(iterable, skipchar or matr.defaultskipchar))
 
     def __rshift__(self, outp):
+        """ self >> outputfile :: writes self to outputfile, """
         if isinstance(outp, str):
             return self >> csv.writer(open(outp, 'w'))
         if __debug__:
@@ -52,9 +53,61 @@ class matr(npmatr):
                 return NotImplemented
         return self
 
+    def __getitem__(self, val):
+        return super().__getitem__(self._transrowcol(*val if isinstance(val, tuple) else (val, slice(None))))
+
+    def __setitem__(self, val, toset):
+        return super().__setitem__(self._transrowcol(*val if isinstance(val, tuple) else (val, slice(None))), toset)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        print('todo;exit')
+        # if type is None:
+            # if self.file != None:
+                # return self.__rshift__(self.file)
+        return True
+    def _npmatrfromfile(iterable, skipchar):
+        mdata = []
+        if __debug__:
+            assert hasattr(iterable, '__iter__') or hasattr(iterable, '__next__')
+        if hasattr(iterable, '__iter__'):
+            for line in iterable:
+                if not line[0][0] == skipchar:
+                    mdata.append(line)
+        elif hasattr(iterable, '__next__'):
+            while iterable.hasnext():
+                line = next(iterable)
+                if not line[0] == skipchar:
+                    mdata.append(line)
+        return mdata
+
+    def _transrowcol(self, row, col=slice(None)):
+        """ translates rows and columns into their respective strings..."""
+        try:
+            if isinstance(row, str):
+                row = self[:,0].flatten().tolist()[0].index(row)
+            if isinstance(col, str):
+                col = self[0,].flatten().tolist()[0].index(col)
+            return (row, col)
+        except ValueError:
+            raise
 
 def main():
-    df = 'testdata.txt' >> matr()
-    df >> 'testdata.txt'
+    with matr('testdata.txt') as m:
+        m['ant','legs'] = 8
+        print('mmm:',m)
+    m = 'testdata.txt' >> matr()
+    print(m)
+    # m['ant','legs'] = 6
+    # m >> 'testdata.txt'
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
