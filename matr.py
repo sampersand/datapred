bedcfb9ed7a14c4565e3eac40f9db974624137b0
westerhack <westerhack@gmail.com>
2016-02-06 18:57:33 -0800

    going to add headers and ids. going to have to re-do some stuff to make that work, however...

--- matr.py
+++ matr.py
@@ -9,9 +9,9 @@ class matr(npmatr):
 
     defaultskipchar = '#'
 
-    def __new__(self, file = None, data = [], skipchar = None):
+    def __new__(self, file = None, dtype = float, data = [], skipchar = None):
         if file:
-            ret = matr().__rrshift__(file)
+            ret = matr().fromfile(file, dtype = dtype)
         else:
             ret = super().__new__(self, data)    
         ret.file = file
@@ -19,38 +19,20 @@ class matr(npmatr):
 
     def __str__(self):
         return 'file \'{}\':\n{}'.format(self.file, super().__str__())
-    def __lshift__(self, inp):
-        return self.__rrshift__(inp)
 
-    def __rlshift__(self, outp):
-        return self.__rshift__(outp)
+    def __lshift__(self, fout):
+        return self.__rrshift__(fout)
 
-    def __rrshift__(self, iterable, skipchar = None):
-        """ inputfile >> self :: Sets self to the matrix read from the input file"""
-        if isinstance(iterable, str):
-            ret = self.__rrshift__(csv.reader(open(iterable)))
-            ret.file = iterable #its a string
-            return ret
-        elif not hasattr(iterable, '__iter__') and not hasattr(iterable, '__next__'):
-            return NotImplemented
-        return matr(data = matr._npmatrfromfile(iterable, skipchar or matr.defaultskipchar))
+    def __rlshift__(self, fout):
+        return self.__rshift__(fout)
 
-    def __rshift__(self, outp):
-        """ self >> outputfile :: writes self to outputfile, """
-        if isinstance(outp, str):
-            return self >> csv.writer(open(outp, 'w'))
-        if __debug__:
-            assert hasattr(outp, "writerow") or hasattr(outp, "write") or hasattr(outp, "writeline")
-        for row in self.tolist():
-            if hasattr(outp, "writerow"): #AKA, if it's a csv writer.
-                outp.writerow(row)
-            elif hasattr(outp, "writeline"):
-                outp.write(",".join(row))
-            elif hasattr(outp, "write"):
-                outp.write(",".join(row) + '\n')
-            else:
-                return NotImplemented
-        return self
+    def __rrshift__(self, fin):
+        """ fin >> self :: Sets self to the matrix read from the input file"""
+        return self.fromfile(fin)
+
+    def __rshift__(self, fout):
+        """ self >> fout :: writes self to fout, """
+        return self.tofile(fout)
 
     def __getitem__(self, val):
         return super().__getitem__(self._transrowcol(*val if isinstance(val, tuple) else (val, slice(None))))
@@ -62,24 +44,24 @@ class matr(npmatr):
         return self
 
     def __exit__(self, type, value, traceback):
-        if type is None and self.file != None:
-            return self.__rshift__(self.file)
+        if type is None:
+            if self.file != None:
+                return self.__rshift__(self.file)
+        else:
+            raise type(value, traceback)
         return True
 
-    def __invert__(self):
-        pass
-
-    def _npmatrfromfile(iterable, skipchar):
+    def _npmatrfromfile(fin, skipchar, dtype):
         mdata = []
         if __debug__:
-            assert hasattr(iterable, '__iter__') or hasattr(iterable, '__next__')
-        if hasattr(iterable, '__iter__'):
-            for line in iterable:
+            assert hasattr(fin, '__iter__') or hasattr(fin, '__next__')
+        if hasattr(fin, '__iter__'):
+            for line in fin:
                 if not line[0][0] == skipchar:
-                    mdata.append(line)
-        elif hasattr(iterable, '__next__'):
-            while iterable.hasnext():
-                line = next(iterable)
+                    mdata.append([dtype(ele) for ele in line])
+        elif hasattr(fin, '__next__'):
+            while fin.hasnext():
+                line = next(fin)
                 if not line[0] == skipchar:
                     mdata.append(line)
         return mdata
@@ -95,9 +77,38 @@ class matr(npmatr):
         except ValueError:
             raise
 
+    def fromfile(self, fin, skipchar = None, dtype = None):
+        if isinstance(fin, str):
+            ret = self.fromfile(csv.reader(open(fin)))
+            ret.file = fin #its a string
+            return ret
+        elif not hasattr(fin, '__iter__') and not hasattr(fin, '__next__'):
+            return NotImplemented
+        return matr(data = matr._npmatrfromfile(fin, skipchar or matr.defaultskipchar, dtype))
+
+    def tofile(self, fout):
+        if isinstance(fout, str):
+            return self.tofile(csv.writer(open(fout, 'w')))
+        if __debug__:
+            assert hasattr(fout, "writerow") or hasattr(fout, "write") or hasattr(fout, "writeline")
+        for row in self.tolist():
+            if hasattr(fout, "writerow"): #AKA, if it's a csv writer.
+                fout.writerow(row)
+            elif hasattr(fout, "writeline"):
+                fout.write(",".join(row))
+            elif hasattr(fout, "write"):
+                fout.write(",".join(row) + '\n')
+            else:
+                return NotImplemented
+        return self
+
+
 def main():
     with matr('testdata.txt') as m:
-        print(~m)
+        # m2 = npmatr('1,2,3;4,5,6;7,8,9')
+        print(m)
+        # print(m+m2)
+        # print(m2)
 if __name__ == '__main__':
     main()
 
